2f2aa8387a71476d2f56a69293332402e5240a10
nitroFamily <samaydanov@gmail.com>
2016-05-08 12:27:38 +0300

    extended test for add template

--- test/integration/tests/add-template/Main.hs
+++ test/integration/tests/add-template/Main.hs
@@ -3,7 +3,12 @@ import System.Directory
 
 main :: IO ()
 main = do
+    curDir <- getCurrentDirectory
+    let prj1 = curDir ++ "/prj1"
+        prj2 = curDir ++ "/prj2"
+
     -- Fail add
+    setCurrentDirectory prj1
     bimoFailAndStderrContent ["add", "-t", "new-template"]
                              [ "Not found model exec"
                              , "Build model before add"
@@ -17,4 +22,11 @@ main = do
     -- Fail when template already exists
     bimoFailAndStderrContent ["add", "-t", "new-template"]
                              ["Template with this name already exists"]
+
+    -- Fail when template model already exists
+    setCurrentDirectory prj2
+    bimo ["build", "-p"]
+    bimoFailAndStderrContent ["add", "-t", "new-template2"]
+                             ["Model with this name already exists"]
+
 
